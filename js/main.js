8435c60abe8deb555fc6f42222b3ff4316389a8d
vacer1997 <70571233+vacer1997@users.noreply.github.com>
2021-01-18 11:22:26 +0100

    js uitgebreid

--- js/main.js
+++ js/main.js
@@ -24,3 +24,23 @@ let wordStatus = null;
 function eenWord() {
   antwoord = programming_languages[Math.floor(Math.random() * programming_languages.length)];
 }
+
+function knoppen() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+        <button
+         class="btn btn-lg btn-primary m-2"
+         id='` + letter + `
+         onClick="handleGuess('` + letter + `')"
+         >
+         ` + letter + `
+         </button>
+       `).join('');
+   
+     document.getElementById('keyboard').innerHTML = buttonsHTML;
+   }
+
+document.getElementById('maxFouten').innerHTML = maxFouten;
+
+eenWord();
+knoppen();
