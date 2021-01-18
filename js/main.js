51d460e05d20452713de737eb7d5947b4b258fd5
vacer1997 <70571233+vacer1997@users.noreply.github.com>
2021-01-18 12:18:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function knoppen() {
     `
         <button
          class="btn btn-lg btn-primary m-2"
-         id='` + letter + `
+         id='` + letter + `'
          onClick="handleGuess('` + letter + `')"
          >
          ` + letter + `
@@ -40,10 +40,28 @@ function knoppen() {
      document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
+function handleGuess(gekozenLetter) {
+    pogingen.indexOf(gekozenLetter) === -1 ? pogingen.push(gekozenLetter) : null;
+    document.getElementById(gekozenLetter).setAttribute('disabled', true);
+
+    alert(antwoord);
+
+    if (antwoord.indexOf(gekozenLetter) >= 0) {
+        gegokteWoord();
+    } else if (antwoord.indexOf(gekozenLetter) === -1) {
+        fouten++;
+        updateFouten();
+    }
+}
+
 function gegokteWoord() {
     wordStatus = antwoord.split('').map(letter => (pogingen.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
     document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateFouten() {
+    document.getElementById('fouten').innerHTML = fouten;
 }
 
 document.getElementById('maxFouten').innerHTML = maxFouten;
